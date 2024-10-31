cec7ee3dbe21696958a08d984bcd710e8c1e6373
edwinhati <edwinhati@outlook.com>
2024-11-01 00:16:28 +0700

    initial commit

---
+++ src/test/java/com/ruker/workspace/WorkspaceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ruker.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+   // TODO document why this method is empty
+ }
+
+}
